2ea4529f3fdfc43d71e12f8f6d806dc3d2b81ce0
Ryo Utsunomiya <millionsnowliving@gmail.com>
2024-07-11 20:41:52 +0900

    Add abc170

---
+++ src/bin/abc170_a.rs
@@ -0,0 +1,20 @@
+fn main() {
+    proconio::input! {
+        x1: i32,
+        x2: i32,
+        x3: i32,
+        x4: i32,
+        x5: i32,
+    }
+    if x1 == 0 {
+        println!("1")
+    } else if x2 == 0 {
+        println!("2")
+    } else if x3 == 0 {
+        println!("3")
+    } else if x4 == 0 {
+        println!("4")
+    } else if x5 == 0 {
+        println!("5")
+    }
+}
---
+++ src/bin/abc170_b.rs
@@ -0,0 +1,13 @@
+fn main() {
+    proconio::input! {
+        x: i32,
+        y: i32,
+    }
+    let mut ans = false;
+    for num_two_leg_animals in 0..x+1 {
+        if num_two_leg_animals * 2 + (x - num_two_leg_animals) * 4 == y {
+            ans = true;
+        }
+    }
+    println!("{}", if ans { "Yes" } else { "No" })
+}
